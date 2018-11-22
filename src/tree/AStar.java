6919e36172ef1293811d046f155d1b0bde027822
korrupterkurde <37101345+aaronkurda@users.noreply.github.com>
2018-11-22 13:52:51 +0100

    Ostfalia Session

--- src/tree/AStar.java
+++ src/tree/AStar.java
@@ -1,17 +1,69 @@
 package tree;
-import java.util.List;
+import java.util.*;
 
 import tree.data.EntityData;
 
 public class AStar<T extends EntityData> {
+    public static final Node MAXIMUM_COST = new Node(null, null, 0, Integer.MAX_VALUE);
+
 	private List<T> data;
 	private Tree<T> tree;
+	private Node<T> MinLeaf;
 	
 	public AStar(List<T> data) {
+	    data.sort(new Comparator<T>() {
+            @Override
+            public int compare(T o1, T o2) {
+                if(o1.getTime() < o2.getTime()) {
+                    return -1;
+                } else if(o1.getTime() > o2.getTime()) {
+                    return 1;
+                }
+                return 0;
+            }
+        });
 		this.data = data;
 		tree = new Tree<>(data);
-		tree.expandAll();
+		lazyExpand();
 	}
+
+	public void lazyExpand() {
+        List<Node<T>> knoten = new ArrayList<>();
+        knoten.addAll(tree.getRoot().expand(data));
+
+        Node<T> currentMin = MAXIMUM_COST;
+        int tiefe = 2;
+        while(!knoten.isEmpty()) {
+            knoten = expandAll(knoten);
+            knoten.sort(new Comparator<Node<T>>() {
+                @Override
+                public int compare(Node<T> o1, Node<T> o2) {
+                    if(o1.getfCost() < o2.getfCost()) {
+                        return -1;
+                    } else if(o1.getfCost() > o2.getfCost()) {
+                        return 1;
+                    }
+                    return 0;
+                }
+            });
+            knoten.subList(data.size() * (data.size() - 1) -1, knoten.size()-1).clear();
+            tiefe *= 2;
+            currentMin = knoten.get(0);
+            if(currentMin.getParent().getChildren().size() == 1) {
+                MinLeaf = currentMin;
+                return;
+            }
+        }
+        return;
+    }
+
+    private List<Node<T>> expandAll(List<Node<T>> knoten) {
+        List<Node<T>> children = new ArrayList<>();
+	    for(Node<T> t: knoten) {
+	        children.addAll(t.expand(data));
+        }
+        return children;
+    }
 
 	public List<Node<T>> getAllNodes() {
 	    return tree.toList();
@@ -29,7 +81,7 @@ public class AStar<T extends EntityData> {
 
     public Node<T> getMinCostLeafNode() {
         List<Node<T>> leafs = getLeafNodes();
-        Node<T> currentMinLeaf = leafs.get(0);
+        Node<T> currentMinLeaf =MinLeaf;
         for(Node<T> leaf: leafs) {
             if(currentMinLeaf.getfCost() > leaf.getfCost()) {
                 currentMinLeaf = leaf;
@@ -39,7 +91,7 @@ public class AStar<T extends EntityData> {
     }
 
     public List<Node<T>> getMinCostPath() {
-	    return tree.getPathToRoot(getMinCostLeafNode());
+	    return tree.getPathToRoot(MinLeaf);
     }
 
     @Override
--- src/util/Main.java
+++ src/util/Main.java
@@ -5,38 +5,20 @@ import tree.data.GuestEntityData;
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException, InterruptedException {
+		long startTime = System.currentTimeMillis();
 		FileDataReader fdr = new FileDataReader(new File("C:\\Uni\\C++\\AStarGuest\\Badbelegungsplanung2"));
 		List<GuestEntityData> dsis = fdr.read();
 
-		/*System.out.println(dsis.toString());
-		Tree<GuestEntityData> t = new Tree<GuestEntityData>(dsis);
-
-		t.expandAll();
-		List<Node<GuestEntityData>> list = t.toList();
-		Node<GuestEntityData> minNode = null;
-		int minCost = Integer.MAX_VALUE;
-		for(Node<GuestEntityData> node: list) {
-		    if(node.isLeaf() && node.getfCost() < minCost) {
-		        minCost = node.getfCost();
-		        minNode = node;
-            }
-            //System.out.println(node.getValue().getName());
-        }
-
-        System.out.println(t.toList());
-
-        List<Node<GuestEntityData>> path = t.getPath(minNode);
-		System.out.println(minNode.getfCost());
-        for(Node<GuestEntityData> tn: path) {
-            System.out.print(tn.getValue().getName() + "-->");
-        }*/
 
 		AStar<GuestEntityData> astar = new AStar<>(dsis);
 		//System.out.println(astar.getMinCostLeafNode());
         System.out.println(astar.getMinCostPath());
+		//TimeUnit.SECONDS.sleep(1);
+		System.out.println((System.currentTimeMillis() - startTime) / 1000.0);
 	}
 
 }
